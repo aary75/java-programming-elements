7451779d96f66ae2dd8677c2d8ac79c688de60e2
Aaryan <aaryankgarg@gmail.com>
2025-02-28 10:12:33 +0530

    [Aaryan] Cal:Calculating average marks of Sam in PCM

---
+++ AverageInPCM.java
@@ -0,0 +1,20 @@
+// Create a class AverageInPCM to calcualate average marks of Sam in PCM
+
+public class AverageInPCM{
+   public static void main(String[] args){
+
+      // Create a variable mathMarks to show the Sam's marks in math out of 100
+     int mathMarks = 94;
+
+      // Create a variable physicsMarks to show the Sam's marks in physics out of 100       
+      int physicsMarks = 95;
+
+      // Create a variable chemistryMarks tio show the Sam's marks in chemistry out or 100
+      int chemistryMarks = 96;
+
+       // Create a variable averageMark to calculate average marks of Sam in PCM
+      int averageMark = (mathMarks + physicsMarks + chemistryMarks)/3;
+
+      // Print the average marks of Sam
+      System.out.print("Sam's average mark in PCM is " + averageMark);
+}}
